e406dd01a9386b4dd1e0545325f8ac1d0424d3ba
nebulaM <nebulam12@gmail.com>
2017-01-28 20:43:12 -0800

    add dependencies for a1

--- init.sh
+++ init.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #required python environment, run as root
+#a0
 sudo apt-get install -y libfreetype6-dev python-pip
 sudo apt-get install -y libblas-dev
 sudo apt-get install -y liblapack-dev
 sudo apt-get install -y gfortran
 sudo pip install numpy scipy
+#a1
+sudo pip install utils
+sudo apt-get install -y python-matplotlib
+sudo pip install scikit-learn
